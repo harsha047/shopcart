1f153760a40e0bdb100d5bf5a40508395b470b73
harsha047 <61135835+harsha047@users.noreply.github.com>
2020-04-02 18:03:29 +0530

    Add files via upload

---
+++ shop cart/routes/cart.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const CartController = require('../controllers/CartController');
+
+router.get('/', CartController.get);
+router.post('/add', CartController.add);
+router.post('/subtract', CartController.subtract);
+module.exports = router;
